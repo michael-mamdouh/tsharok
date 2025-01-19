6d91616fe17083996eea0663c600fa3c973e6cb7
MichaelMamdouh <MMamdouh@singlestop.org>
2025-01-19 23:04:49 +0200

    Implement OTP success page with new layout, styling, and internationalization support

--- src/app/auth/otp-success/otp-success.component.ts
+++ src/app/auth/otp-success/otp-success.component.ts
@@ -16,60 +16,8 @@ import { TranslateComponent } from '../../components/translate/translate.compone
     MatIconModule,
     TranslateComponent
   ],
-  template: `
-    <div class="success-container">
-      <mat-card>
-        <mat-card-content>
-          <div class="success-icon">
-            <mat-icon color="primary">check_circle</mat-icon>
-          </div>
-          <h2><app-translate key="auth.otpSuccess.title"></app-translate></h2>
-          <p><app-translate key="auth.otpSuccess.message"></app-translate></p>
-          <button mat-raised-button color="primary" (click)="goToLogin()">
-            <app-translate key="auth.otpSuccess.loginButton"></app-translate>
-          </button>
-        </mat-card-content>
-      </mat-card>
-    </div>
-  `,
-  styles: [`
-    .success-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 2rem;
-
-      mat-card {
-        width: 100%;
-        max-width: 400px;
-        text-align: center;
-      }
-
-      .success-icon {
-        margin: 2rem 0;
-        
-        mat-icon {
-          font-size: 4rem;
-          width: 4rem;
-          height: 4rem;
-        }
-      }
-
-      h2 {
-        margin-bottom: 1rem;
-        color: #333;
-      }
-
-      p {
-        margin-bottom: 2rem;
-        color: #666;
-      }
-
-      button {
-        width: 100%;
-      }
-    }
-  `]
+  templateUrl: './otp-success.component.html',
+  styleUrls: ["../login/login.component.scss", "./otp-success.component.scss"]
 })
 export class OtpSuccessComponent {
   constructor(private router: Router) {}
