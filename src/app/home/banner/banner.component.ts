3dd747676a5a17be9007a4dbeafc2187ca6c08ee
MichaelMamdouh <MMamdouh@singlestop.org>
2025-02-09 17:57:08 +0200

    Add banner component with internationalization support and styling to home page

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { TranslateComponent } from '../../components/translate/translate.component';
-
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { TranslateComponent } from "../../components/translate/translate.component";
+import { LanguageService } from "../../services/language.service";
 
 @Component({
-  selector: 'app-banner',
+  selector: "app-banner",
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink, 
+    RouterLink,
     MatButtonModule,
     MatIconModule,
-    TranslateComponent
+    TranslateComponent,
   ],
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  templateUrl: "./banner.component.html",
+  styleUrls: ["./banner.component.scss"],
 })
 export class BannerComponent {
-
+  constructor(private langService: LanguageService) {}
+  getTranslatedText(key: string): string {
+    return TranslateComponent.translateValue(key, this.langService);
+  }
 }
