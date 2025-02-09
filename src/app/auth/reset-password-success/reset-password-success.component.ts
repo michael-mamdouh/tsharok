b4774ae62b607bb482b80b7ab62252e297398ab1
MichaelMamdouh <MMamdouh@singlestop.org>
2025-02-09 17:07:53 +0200

    Refactor reset password success component to use external template and styles; add internationalization support for success messages

--- src/app/auth/reset-password-success/reset-password-success.component.ts
+++ src/app/auth/reset-password-success/reset-password-success.component.ts
@@ -16,64 +16,28 @@ import { TranslateComponent } from '../../components/translate/translate.compone
     MatIconModule,
     TranslateComponent
   ],
-  template: `
-    <div class="success-container">
-      <mat-card>
-        <mat-card-content>
-          <div class="success-icon">
-            <mat-icon color="primary">check_circle</mat-icon>
-          </div>
-          <h2>
-            <app-translate key="auth.resetPasswordSuccess.title"></app-translate>
-          </h2>
-          <p>
-            <app-translate key="auth.resetPasswordSuccess.message"></app-translate>
-          </p>
-          <button mat-raised-button color="primary" (click)="goToLogin()">
-            <app-translate key="auth.resetPasswordSuccess.loginButton"></app-translate>
-          </button>
-        </mat-card-content>
-      </mat-card>
-    </div>
-  `,
-  styles: [`
-    .success-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 2rem;
-
-      mat-card {
-        width: 100%;
-        max-width: 400px;
-        text-align: center;
-      }
-
-      .success-icon {
-        margin: 2rem 0;
-        
-        mat-icon {
-          font-size: 4rem;
-          width: 4rem;
-          height: 4rem;
-        }
-      }
-
-      h2 {
-        margin-bottom: 1rem;
-        color: #333;
-      }
-
-      p {
-        margin-bottom: 2rem;
-        color: #666;
-      }
-
-      button {
-        width: 100%;
-      }
-    }
-  `]
+  templateUrl: './reset-password-success.component.html',
+  styleUrls: ["../login/login.component.scss", "../otp-success/otp-success.component.scss"]
+  // template: `
+  //   <div class="success-container">
+  //     <mat-card>
+  //       <mat-card-content>
+  //         <div class="success-icon">
+  //           <mat-icon color="primary">check_circle</mat-icon>
+  //         </div>
+  //         <h2>
+  //           <app-translate key="auth.resetPasswordSuccess.title"></app-translate>
+  //         </h2>
+  //         <p>
+  //           <app-translate key="auth.resetPasswordSuccess.message"></app-translate>
+  //         </p>
+  //         <button mat-raised-button color="primary" (click)="goToLogin()">
+  //           <app-translate key="auth.resetPasswordSuccess.loginButton"></app-translate>
+  //         </button>
+  //       </mat-card-content>
+  //     </mat-card>
+  //   </div>
+  // `
 })
 export class ResetPasswordSuccessComponent {
   constructor(private router: Router) {}
